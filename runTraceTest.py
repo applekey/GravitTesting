03ecf3194baeb08c5a7e290c1801fccaaa327b32
Vincent Chen <applekey@gmail.com>
2016-03-11 11:15:17 -0800

    testing

--- runTraceTest.py
+++ runTraceTest.py
@@ -33,7 +33,13 @@ def textDiff(file1, file2):
     with open(file1) as f1, open(file2) as f2:
         for x,y in zip(f1,f2):
             ## do some matching here
-            print("{0}\t{1}".format(x.strip(),y.strip()))
+            f1Vals = x.strip().split(',')
+            f2Vals = y.strip().split(',')
+            f1Vals = [ float(f1val) for f1val in f1Vals]
+            f2Vals = [ float(f2val) for f2val in f2Vals]
+            for i in range(len(f1Vals)):
+                if f1Vals[i] == f2Vals[i]:
+                    print 'difference, file1: '+str(f1Vals[i]) + ' , file 2: ' + str(f2Vals[i])
 
 
 def testLayer(obj, layer):
@@ -52,4 +58,8 @@ class MyTest(unittest.TestCase):
 
 
 if __name__ == '__main__':
+    file1 = '/home/users/applekey/documents/gravit/build/validation'
+    file2 = '/home/users/applekey/documents/gravit/build/validation'
+    textDiff(file1,file2)
+    exit()
     unittest.main()
